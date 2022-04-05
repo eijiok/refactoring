b9617b1de171daa00231b4ca351725a17df5e434
Eiji Okuda <fabio.okuda@loggi.com>
2022-04-04 22:34:21 -0300

    extract frequentRenterPoints

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -22,11 +22,9 @@ public class Customer {
         String result = "Rental Record for " + getName() + "\n";
 
         for (Rental each : this.rentals) {
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints++;
+            int tempFrequentRenterPoints = getFrequentRenterPoints(each);
+
+            frequentRenterPoints += tempFrequentRenterPoints;
 
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + each.getCharge() + "\n";
@@ -36,6 +34,12 @@ public class Customer {
         result += "Amount owed is " + totalAmount + "\n";
         result += "You earned " + frequentRenterPoints + " frequent renter points";
         return result;
+    }
+
+    private int getFrequentRenterPoints(Rental each) {
+        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+            return 2;
+        return 1;
     }
 
 }
