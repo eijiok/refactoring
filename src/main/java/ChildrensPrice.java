da8849e46c207acb02d7453b685a91f403835e30
Eiji Okuda <fabio.okuda@loggi.com>
2022-04-04 23:15:44 -0300

    implemented getCharge of ChildrensPrice

--- src/main/java/ChildrensPrice.java
+++ src/main/java/ChildrensPrice.java
@@ -3,4 +3,10 @@ public class ChildrensPrice extends Price {
     int getPriceCode() {
         return Movie.CHILDRENS;
     }
+
+    double getCharge(int daysRented) {
+        if (daysRented <= 3) return 1.5;
+
+        return (daysRented - 2) * 1.5;
+    }
 }
