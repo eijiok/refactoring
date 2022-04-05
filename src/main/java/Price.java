2b23f0bf8bbc784a0f3534f92791c1a313ac8438
Eiji Okuda <fabio.okuda@loggi.com>
2022-04-04 23:20:26 -0300

    change getCharge of Price to abstract

--- src/main/java/Price.java
+++ src/main/java/Price.java
@@ -1,19 +1,5 @@
 public abstract class Price {
     abstract int getPriceCode();
 
-    double getCharge(int daysRented) {
-        double result = 0;
-        //determine amounts for each line
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-        }
-        return result;
-    }
+    abstract double getCharge(int daysRented);
 }
