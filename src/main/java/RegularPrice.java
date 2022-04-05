b103a2745fa5ebeb7f38b032e9f3f29de9f11bea
Eiji Okuda <fabio.okuda@loggi.com>
2022-04-04 23:19:06 -0300

    implemented getCharge of RegularPrice

--- src/main/java/RegularPrice.java
+++ src/main/java/RegularPrice.java
@@ -3,4 +3,10 @@ public class RegularPrice extends Price {
     int getPriceCode() {
         return Movie.REGULAR;
     }
+
+    double getCharge(int daysRented) {
+        if (daysRented <= 2) return 2;
+
+        return 2 + (daysRented - 2) * 1.5;
+    }
 }
